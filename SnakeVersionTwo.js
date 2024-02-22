2c3934a09d440df6fe89f717d53a8d091287024a
Soodeh84 <soodeh.mousavi.84@gmail.com>
2024-02-22 11:00:47 +0100

    add snake body parts and the movements, fixing the shadows

--- SnakeVersionTwo.js
+++ SnakeVersionTwo.js
@@ -1,6 +1,8 @@
+"use strict";
+
 import * as THREE from "three"
 import {OrbitControls} from "three/addons/controls/OrbitControls.js";
-//import {TrackballControls} from "three/addons/controls/TrackballControls.js";
+import {TrackballControls} from "three/addons/controls/TrackballControls.js";
 import {OBJLoader} from "three/addons/loaders/OBJLoader.js";
 
 
@@ -15,9 +17,9 @@ renderer.shadowMap.enabled = true;
 renderer.shadowMap.type = THREE.VSMShadowMap;
 // create scene and camera
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(40, canvas.width / canvas.height,
+const camera = new THREE.PerspectiveCamera(50, canvas.width / canvas.height,
                                             0.1, 1000);
-camera.position.x =25;
+camera.position.x =15;
 camera.position.z =0;
 camera.position.y =10;
 
@@ -31,12 +33,14 @@ const ambientLight = new THREE.AmbientLight('#ffffff');
 scene.add(ambientLight);
 
 const spotLight = new THREE.SpotLight('#bbbbbb');
-spotLight.position.set( 4,10, 0);
+spotLight.position.set(5,10, 0);
 spotLight.castShadow = true;
-spotLight.shadow.camera.fov = 40;
-spotLight.shadow.camera.near = 5;
-spotLight.shadow.camera.far = 200;
-spotLight.intensity = 400;
+spotLight.shadow.mapSize.width = 1024;
+spotLight.shadow.mapSize.height = 1024;
+spotLight.shadow.camera.fov = 50;
+spotLight.shadow.camera.near = 6;
+spotLight.shadow.camera.far = 100;
+spotLight.intensity = 500;
 
 scene.add(spotLight);
 
@@ -60,10 +64,6 @@ scene.add( groundMesh );
 
 /******field******************************************************************************************************************/
 const fieldSide = 12;
-const fieldHeight = 12;
-
-
-//const divField = 12; //to divide the playing field
 
 const fieldTexture = txtLoader.load('resources/FloorsCheckerboard_S_Diffuse.jpg');
 fieldTexture.wrapS = THREE.RepeatWrapping;  // wrap mode in u-direction
@@ -75,8 +75,7 @@ normalMap.wrapS = THREE.RepeatWrapping;  // wrap mode in u-direction
 normalMap.wrapT = THREE.RepeatWrapping;  // wrap mode in v-direction
 normalMap.repeat.set(2,2);
 normalMap.magFilter = THREE.LinearMipmapLinearFilter;
-//const fieldGeo = myPlaneGeo();
-const fieldGeo = new THREE.PlaneGeometry(fieldSide,fieldHeight);
+const fieldGeo = new THREE.PlaneGeometry(fieldSide,fieldSide);
 const fieldMesh = new THREE.Mesh(fieldGeo,
                                  new THREE.MeshPhongMaterial({color: "#bbbbbb",
                                                                  map: fieldTexture,
@@ -119,6 +118,7 @@ for (let i=0; i<cushionsT_B.length; i++){
   cushionMeshA.rotation.x = -Math.PI/2;
   cushionMeshA.position.set(cushionsT_B[i].x,cushionsT_B[i].y,cushionsT_B[i].z);
   cushionMeshA.castShadow = true;
+  cushionMeshA.receiveShadow = true;
   scene.add(cushionMeshA);
 }
 //left and right
@@ -137,6 +137,7 @@ for (let i=0; i<cushionsL_R.length; i++){
   cushionMeshB.rotation.x = Math.PI/2;
   cushionMeshB.position.set(cushionsL_R[i].x,cushionsL_R[i].y,cushionsL_R[i].z);
   cushionMeshB.castShadow = true;
+  cushionMeshB.receiveShadow = true;
   scene.add(cushionMeshB);
 }
 /*****************************************************SKY***************************************/
@@ -160,7 +161,7 @@ let skyBoxGeo = new THREE.BoxGeometry(300, 300, 300);
 let skyBox = new THREE.Mesh(skyBoxGeo, matArray);
 scene.add(skyBox);
 /************************************snake head******************************************************************* */
-const snakeTexture = txtLoader.load('resources/lavatile.jpg');
+const snakeTexture = txtLoader.load('resources/snakeSkin.jpg');
 const blockSize = 1;
 const snakeX = (blockSize * 0.95);
 const snakeY = (blockSize * 0.95);
@@ -172,6 +173,7 @@ const headMat = new THREE.MeshBasicMaterial( {color: 0x008000,
 const snakeHead = new THREE.Mesh( headGeo, headMat ); 
 
 scene.add(snakeHead);
+snakeHead.castShadow = true;
 
 //randomly place the snakeHead on a unit cell of the playing field 
 function placeHead() {
@@ -179,81 +181,47 @@ function placeHead() {
   const headX = Math.floor(Math.random() * fieldSide - fieldSide / 2) * blockSize + blockSize / 2;
   const headZ = Math.floor(Math.random() * fieldSide - fieldSide / 2) * blockSize + blockSize / 2;
   
-  snakeHead.position.set(headX, 0.6, headZ);
+  snakeHead.position.set(headX, 0.5, headZ);
 }
 placeHead();
-//FUCKING ROUNDED CUBES!.........................
-/* const length = 0.8, width = 0.8;
-
-const shape = new THREE.Shape();
-shape.moveTo( 0,0 );
-shape.lineTo( 0, width );
-shape.lineTo( length, width );
-shape.lineTo( length, 0 );
-shape.lineTo( 0, 0 );
-
-const extrudeSettings = {
-	depth: 0.9, 
-	bevelEnabled: true,
-	bevelSegments: 18
-};
-const geometry = new THREE.ExtrudeGeometry( shape, extrudeSettings );
-const material = new THREE.MeshBasicMaterial( { color: 0x0000ff } );
-const mesh = new THREE.Mesh( geometry, material ) ;
-scene.add( mesh );
-mesh.castShadow = true;
-
-function placeHeadB() {
-
-  const lengthX = Math.floor(Math.random() * 11 - 11 / 2)* blockSize + blockSize / 2;
-  const widthZ = Math.floor(Math.random() * 11 - 11 / 2)* blockSize + blockSize / 2;
-  
-  mesh.position.set(lengthX, 0.2, widthZ);
-}
-placeHeadB(); */
 /********************************APPLE******************************************************************* */
-const ScaleFact = 0.01;
-let foodX;
-let foodZ;
+const scaleFact = 0.01;
+let appleObj;
 const loader = new OBJLoader();
 
-txtLoader.load(
-    'resources/Apple_BaseColor.png',
-    function (texture) {
-      
-        // Load the normal map
-        txtLoader.load(
-            'resources/Apple_Normal.png',
-            function (normalMap) {
+
+txtLoader.load('resources/Apple_BaseColor.png', function (texture) {
+      // Load the normal map
+        txtLoader.load('resources/Apple_Normal.png', function (normalMap) {
                 // Load the specular map
-                txtLoader.load(
-                    'resources/Apple_Roughness.png',
-                    function (specularMap) {
+                txtLoader.load('resources/Apple_Roughness.png', function (specularMap) {
                         // Set up material with the loaded texture, normal map, and specular map
                         const material = new THREE.MeshStandardMaterial({ 
                             map: texture,
                             normalMap: normalMap,
                             specularMap: specularMap,
                         });
-
                         // Load the OBJ file
-                        loader.load(
-                            'resources/Apple.obj',
-                            function (object) {
+                        loader.load('resources/Apple.obj', function (object) {
                                 // Apply the material with the texture to the object
                                 object.traverse(function (child) {
                                     if (child instanceof THREE.Mesh) {
+                                        child.castShadow = true;
                                         child.material = material;
                                     }
                                 });
                                 //scale the Apple
-                                object.scale.set(ScaleFact, ScaleFact, ScaleFact);
+                                //object.scale.set(ScaleFact, ScaleFact, ScaleFact);
                                 //randomly place the apple on the field
-                                foodX = (Math.floor(Math.random() * fieldSide - fieldSide / 2) + blockSize / 2) * blockSize;
-                                foodZ = (Math.floor(Math.random() * fieldSide - fieldSide / 2) + blockSize / 2) * blockSize;
-                                object.position.set(foodX, 0.5, foodZ);
-                                
-                                scene.add(object);
+                                /* foodX = (Math.floor(Math.random() * fieldSide - fieldSide / 2) + blockSize / 2) * blockSize;
+                                foodZ = (Math.floor(Math.random() * fieldSide - fieldSide / 2) + blockSize / 2) * blockSize; */
+                                //object.position.set(foodX, 0.5, foodZ);
+                                //placeFood();
+                                //scene.add(object);
+                                //loadedObjectPosition = object.position.clone();
+                                //placeFood(object);  
+                                appleObj = object;
+                                placeFood();
                             },
                             function (xhr) {
                                 // Progress callback
@@ -293,9 +261,138 @@ txtLoader.load(
         // Error callback for texture loading
         console.error('Error loading texture:', error);
     }
-);
+); 
+
+function placeFood() {
+  let foodX;
+  let foodZ;
+  do{
+  foodX = (Math.floor(Math.random() * fieldSide - fieldSide / 2) + blockSize / 2) * blockSize;
+  foodZ = (Math.floor(Math.random() * fieldSide - fieldSide / 2) + blockSize / 2) * blockSize;
+  } while (findFreeCell(foodX, foodZ));
+  
+  appleObj.scale.set(scaleFact,scaleFact,scaleFact);
+  appleObj.position.set(foodX, 0.49, foodZ);
+  //appleObj.castShadow = true;
+  scene.add(appleObj);
+}
 
 
+//check if the cells are occupied by snake and find an empty cell to place the food
+function findFreeCell(x, y) {//check collision with snakeHead
+  const headPosition = snakeHead.position;
+  if (headPosition.x === x && headPosition.y === y) {
+    return true;
+  }
+  for (const bodyPart of snakeBody) {//check collision with snakeBody
+    const bodyPartPosition = bodyPart.position;
+    if (bodyPartPosition.x === x && bodyPartPosition.y === y) {
+      return true;
+    }
+  }
+  return false;
+}
+/****************************************snake Movement************************************* */
+const snakeBody = [];
+let prevHeadPosition = new THREE.Vector3();
+let gameOver = false;
+let speed = new THREE.Vector3(0, 0, 0); 
+//audio sounds
+const eatBallSound = new Audio ("resources/sound2.wav");
+const gameOverSound = new Audio ("resources/sound1.wav");
+
+function moveSnake() {
+  if(gameOver){
+    return; //Exit function if game is over
+  }
+  prevHeadPosition.copy(snakeHead.position); //store the previous position of the head before it is updated
+  snakeHead.position.add(speed.clone().multiplyScalar(blockSize));//update snakeHead position based on its current position
+
+  // Check for collisions with food
+  if (appleObj && prevHeadPosition.distanceTo(appleObj.position) < 0.5) {
+    eatBallSound.play();
+    snakeTail();
+    //scene.remove(appleObj); 
+    placeFood();
+    
+  }
+  // Check for collisions with boundaries or itself
+  else if (
+    snakeHead.position.x < -fieldSide / 2 ||
+    snakeHead.position.x > fieldSide / 2 ||
+    snakeHead.position.z < -fieldSide / 2 ||
+    snakeHead.position.z > fieldSide / 2 ||
+    checkSelfCollision()
+  ) {
+    gameOverSound.play();
+    alert("OH NoOo, You killed me! I was still hungry: " + snakeBody.length);
+    updateGame();
+  }
+  // Update body parts to attach and follow the head
+  for (let i = snakeBody.length - 1; i > 0; i--) {
+    snakeBody[i].position.copy(snakeBody[i - 1].position);
+  }
+  // Update the first body part to follow the head
+  if (snakeBody.length > 0) {
+    snakeBody[0].position.copy(prevHeadPosition);
+  }
+}
+// Check if the snake collides with itself
+function checkSelfCollision() {
+  for (let i = 0; i < snakeBody.length; i++) {
+    if (snakeHead.position.distanceTo(snakeBody[i].position) < 1) {
+      return true;
+    }
+  }
+  return false;
+}
+
+/*************************************Add snakeBody************************************************************** */
+function snakeTail() {
+  
+  const tailGeo = new THREE.BoxGeometry(snakeX, snakeY, snakeZ);
+  const tailMat = new THREE.MeshStandardMaterial({color: 'pink',
+                                                  map: snakeTexture,
+                                                  transparent: true,
+                                                  opacity: 0.9});
+  const newBodyPart = new THREE.Mesh(tailGeo, tailMat);
+  newBodyPart.castShadow = true;
+  newBodyPart.position.copy(prevHeadPosition); //use the previous position of the head for the body
+  snakeBody.push(newBodyPart); //add the new body part to the front of the array
+
+  scene.add(newBodyPart);  
+}
+/************************Handle arrow key input for snake movement******************************/
+const speedValue = 1;
+document.addEventListener("keydown", ArrowKeysHandler);
+
+function ArrowKeysHandler(event) {
+  //check for each direction that the snake wont go backward 
+  if (event.key === "ArrowUp" && speed.x !== -speedValue) {
+    speed.set(-speedValue,0 , 0);
+  } else if (event.key === "ArrowDown" && speed.x !== speedValue) {
+    speed.set(speedValue, 0, 0);
+  } else if (event.key === "ArrowLeft" && speed.y !== speedValue) {
+    speed.set(0, 0, speedValue);
+  } else if (event.key === "ArrowRight" && speed.y !== -speedValue) {
+    speed.set(0, 0, -speedValue);
+  }
+}
+/****************************Reset the game*******************************************/
+function updateGame() {
+  snakeHead.position.set(0, 0, 0.5);
+  speed.set(0, 0, 0);
+  // Remove snake body parts
+  for (const bodyPart of snakeBody) {
+    scene.remove(bodyPart);
+  }
+  snakeBody.length = 0;
+  //scene.remove(appleObj); 
+  placeHead();
+  placeFood();
+}
+// Set interval for snake movement
+setInterval(moveSnake, 250);
 /**************************************Screen**********************************************************************/
 const screenX = 15;
 const screenZ = 4;
@@ -310,18 +407,16 @@ const screenMesh = new THREE.Mesh( new THREE.PlaneGeometry( screenX, screenZ),
 
 screenMesh.rotation.y = Math.PI/2;
 screenMesh.castShadow = true;
-screenMesh.position.set(-10, 3.5, 0);
+screenMesh.position.set(-10, 2.5, 0);
 scene.add( screenMesh );
 screenMesh.material.needsUpdate = true;
 
 const rtCamera = new THREE.PerspectiveCamera(90, rt.width / rt.height,
                                              0.1, 1000);
-rtCamera.position.copy(snakeHead.position); 
-//rtCamera.position.copy(mesh.position); 
+rtCamera.position.copy(snakeHead.position);  
 /************************************Renderer***************************************** */
 const controls = new OrbitControls( camera, canvas );
-//const controlsb = new TrackballControls( rtCamera, canvas );
-//controls.zoomSpeed = 5;
+//const controlsb = new TrackballControls(rtCamera, renderer.domElement);
 function render() {
   requestAnimationFrame(render);
   
@@ -333,7 +428,7 @@ function render() {
 
   controls.update();
   //controlsb.update();
-  //renderer.render(scene, camera);
+  
 }
 
 render();
